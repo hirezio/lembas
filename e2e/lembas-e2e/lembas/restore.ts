54e576037c32000a48288423f19c101082fce6a4
Shai Reznik <shairez@users.noreply.github.com>
2022-06-07 19:01:31 +0300

    playing with restore hook

--- e2e/lembas-e2e/lembas/restore.ts
+++ e2e/lembas-e2e/lembas/restore.ts
@@ -1,26 +1,39 @@
-import execa from 'execa';
+import * as execa from 'execa';
+import { writeFile, unlink } from 'fs/promises';
 
 export default async function restore(sqlDumpAsString: string) {
-  const bla = `
-    HEY
-    WHATS
-    WRONG
-  `;
+  // const bla = `
+  //   HEY
+  //   WHATS
+  //   WRONG
+  // `;
+
+  // try {
+    
+  //   const { stdout } = await execa('echo', [bla]);
+  //   console.log('stdout', stdout);
+  // } catch (error) {
+  //   console.log('error: ', error);
+  // }
+
+  const TEMP_FILE = './tmp_dump';
+  await writeFile(TEMP_FILE, sqlDumpAsString);
 
   try {
     
-    const { stdout } = await execa('echo', [bla]);
-    console.log('stdout', stdout);
+    await execa('docker-compose',
+      [
+        '-f', '../../demo-server/docker-compose.yml', 'exec', '-T', 'db',
+        'pg_restore', '-c', '--no-acl', '-U', 'postgres', 'mydb', '<', TEMP_FILE
+      ],
+      {cwd: __dirname}
+    );
+    await unlink(TEMP_FILE);
+    
   } catch (error) {
-    console.log('error: ', error);
+    await unlink(TEMP_FILE);
+    throw error;
   }
-
-  // await execa('docker-compose',
-  //   [
-  //     '-f', '../../demo-server/docker-compose.yml', 'exec', '-T', 'db',
-  //     'pg_restore', '-c', '--no-acl', '-U', 'postgres', 'mydb', '|', sqlDumpAsString
-  //   ],
-  //   {cwd: __dirname}
-  // );
+  
 
 }
