84c7211989400db72c8af89bdca7db61babd07a8
Shai Reznik <shairez@users.noreply.github.com>
2022-06-09 01:30:02 +0300

    fix(lembas-cypress): fix setupData cypress task not finding files

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
+* [] FIX: write a test for the single `.lembas` file problem
+* [] Remove duplication between cypress plugin and lembas 's config.ts
 * [] Debugging tool for setup files (run them without cypress)
 * [] `lembas init` - generates "lembas" folder with 3 hook files
 * [] CLI that sets up the hooks based on DB type
--- packages/lembas-cypress/src/lib/setup-data-task.ts
+++ packages/lembas-cypress/src/lib/setup-data-task.ts
@@ -1,12 +1,29 @@
 import * as path from 'path';
+import { existsSync } from 'fs';
 
 export const SETUP_DATA = 'setupData';
+const LEMBAS_CONFIG_FILENAME = `lembas.json`;
 
 export const setupDataTask: any = {};
 
 setupDataTask[SETUP_DATA] = async function setupData(filePathToLoad: string) {
-  const fullPath = path.resolve(process.cwd(), filePathToLoad);
+  const lembasRoot = findLembasRoot(process.cwd());
+  const fullPath = path.resolve(lembasRoot, filePathToLoad);
   const setup = await import(fullPath);
 
   return setup.default();
 };
+
+export function findLembasRoot(dir: string): string {
+  if (existsSync(path.join(dir, LEMBAS_CONFIG_FILENAME))) {
+    return dir;
+  }
+
+  if (path.dirname(dir) === dir) {
+    throw new Error(
+      `There must be a ${LEMBAS_CONFIG_FILENAME} present in the root of the workspace / project`
+    );
+  }
+
+  return findLembasRoot(path.dirname(dir));
+}
